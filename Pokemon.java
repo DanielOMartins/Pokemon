5ca09d929e326bce6b31377b9792255eeb923f4f
DanielOMartins <78224425+DanielOMartins@users.noreply.github.com>
2021-05-10 01:58:28 -0300

    Update Pokemon.java
    arrumei as classes dos pokemons e adicionei uma para se certificar de qual pokemon chamar

--- Pokemon.java
+++ Pokemon.java
@@ -6,13 +6,58 @@ public class Pokemon {
     }
     
     public int selecionarPoke(String nome, int op){
-        if(nome == "charizard"){
-            return atributo = charizard(op);
+        if(nome == "charmander"){
+            return atributo = charmander(op);
+        }
+        if(nome == "hoho"){
+            return atributo = hoho(op);
+        }
+        if(nome == "vulpix"){
+            return atributo = vulpix(op);
+        }
+        if(nome == "magmar"){
+            return atributo = magmar(op);
+        }
+        if(nome == "pikachu"){
+            return atributo = pikachu(op);
+        }
+        if(nome == "ampharos"){
+            return atributo = ampharos(op);
+        }
+        if(nome == "dedenne"){
+            return atributo = dedenne(op);
+        }
+        if(nome == "jolteon"){
+            return atributo = jolteon(op);
+        }
+        if(nome == "bayleef"){
+            return atributo = bayleef(op);
+        }
+        if(nome == "treecko"){
+            return atributo = treecko(op);
+        }
+        if(nome == "shaymin"){
+            return atributo = shaymin(op);
+        }
+        if(nome == "bulbasaur"){
+            return atributo = bulbasaur(op);
+        }
+        if(nome == "totodile"){
+            return atributo = totodile(op);
+        }
+        if(nome == "squirtle"){
+            return atributo = squirtle(op);
+        }
+        if(nome == "lapras"){
+            return atributo = lapras(op);
+        }
+        if(nome == "maril"){
+            return atributo = maril(op);
         }
         return 0;
     }
     //Fogo
-    public int charizard(int op){
+    public int charmander(int op){
         nome = "Charizard";
         tipo = "fogo";
         if(op == 1){return ata = 4000;}
@@ -21,125 +66,142 @@ public class Pokemon {
         if(op == 4){hp = 25000;}
         return 0;
     }
-    public void hoho(){
+    public int hoho(int op){
         nome = "Ho-Ho";
         tipo = "fogo";
-        ata = 10000;
-        def = 6000;
-        agil = 40;
-        hp = 40000;
+        if(op == 1){return ata = 10000;}
+        if(op == 2){return def = 6000;}
+        if(op == 3){return agil = 40;}
+        if(op == 4){hp = 40000;}
+        return 0;
     }
-    public void vulpix(){
+    public int vulpix(int op){
         nome = "Vulpix";
         tipo = "fogo";
-        ata = 3000;
-        def = 5000;
-        agil = 50;
-        hp = 20000;
+        if(op == 1){return ata = 3000;}
+        if(op == 2){return def = 5000;}
+        if(op == 3){return agil = 50;}
+        if(op == 4){hp = 20000;}
+        return 0;
     }
-    public void magmar(){
+    public int magmar(int op){
         nome = "Magmar";
         tipo = "fogo";
-        ata = 5000;
-        def = 4000;
-        agil = 25;
-        hp = 50000;
+        if(op == 1){return ata = 5000;}
+        if(op == 2){return def = 4000;}
+        if(op == 3){return agil = 25;}
+        if(op == 4){hp = 50000;}
+        return 0;
     }
     //Eletricidade
-    public void pikachu(){
+    public int pikachu(int op){
         nome = "Pikachu";
         tipo = "elétrico";
-        ata = 7000;
-        def = 8000;
-        agil = 40;
-        hp = 20000;
+        if(op == 1){return ata = 7000;}
+        if(op == 2){return def = 8000;}
+        if(op == 3){return agil = 40;}
+        if(op == 4){hp = 20000;}
+        return 0;
     }
-    public void ampharos(){
+    public int ampharos(int op){
         nome = "Ampharos";
         tipo = "elétrico";
-        ata = 9000;
-        def = 10000;
-        agil = 35;
-        hp = 50000;
+        if(op == 1){return ata = 9000;}
+        if(op == 2){return def = 10000;}
+        if(op == 3){return agil = 35;}
+        if(op == 4){hp = 50000;}
+        return 0;
     }
-    public void dedenne(){
+    public int dedenne(int op){
         nome = "Dedenne";
         tipo = "elétrico";
-        ata = 4000;
-        def = 4000;
-        agil = 50;
-        hp = 35000;
+        if(op == 1){return ata = 4000;}
+        if(op == 2){return def = 4000;}
+        if(op == 3){return agil = 50;}
+        if(op == 4){hp = 35000;}
+        return 0;
     }
-    public void jolteon(){
+    public int jolteon(int op){
         nome = "Jolteon";
         tipo = "elétrico";
-        ata = 5000;
-        def = 5000;
-        agil = 50;
-        hp = 45000;
+        if(op == 1){return ata = 5000;}
+        if(op == 2){return def = 5000;}
+        if(op == 3){return agil = 50;}
+        if(op == 4){hp = 45000;}
+        return 0;
     }
     //Terra
-    public void bayleef(){
+    public int bayleef(int op){
         nome = "Bayleef";
         tipo = "terra";
-        ata = 9000;
-        def = 6000;
-        agil = 20;
-        hp = 50000;
+        if(op == 1){return ata = 9000;}
+        if(op == 2){return def = 6000;}
+        if(op == 3){return agil = 20;}
+        if(op == 4){hp = 50000;}
+        return 0;
     }
-    public void treecko(){
+    public int treecko(int op){
         nome = "Treecko";
         tipo = "terra";
-        ata = 6000;
-        def = 6000;
-        agil = 35;
-        hp = 35000;
+        if(op == 1){return ata = 6000;}
+        if(op == 2){return def = 6000;}
+        if(op == 3){return agil = 35;}
+        if(op == 4){hp = 35000;}
+        return 0;
     }
-    public void shaymin(){
+    public int shaymin(int op){
         nome = "Shaymin";
         tipo = "terra";
-        ata = 3000;
-        def = 7000;
-        agil = 50;
-        hp = 45000;
+        if(op == 1){return ata = 3000;}
+        if(op == 2){return def = 7000;}
+        if(op == 3){return agil = 50;}
+        if(op == 4){hp = 45000;}
+        return 0;
     }
-    public void bulbasaur(){
+    public int bulbasaur(int op){
         nome = "Bulbasaur";
         tipo = "terra";
-        ata = 7000;
-        def = 2000;
-        agil = 50;
-        hp = 10000;
+        if(op == 1){return ata = 7000;}
+        if(op == 2){return def = 2000;}
+        if(op == 3){return agil = 50;}
+        if(op == 4){hp = 10000;}
+        return 0;
     }
     //Água
-    public void totodile(){
+    public int totodile(int op){
         nome = "Totodile";
         tipo = "água";
-        ata = 7000;
-        def = 3000;
-        agil = 30;
-        hp = 35000;
-    }public void squirtle(){
+        if(op == 1){return ata = 7000;}
+        if(op == 2){return def = 3000;}
+        if(op == 3){return agil = 30;}
+        if(op == 4){hp = 35000;}
+        return 0;
+    }
+    public int squirtle(int op){
         nome = "Squirtle";
         tipo = "água";
-        ata = 6000;
-        def = 5000;
-        agil = 40;
-        hp = 30000;
+        if(op == 1){return ata = 6000;}
+        if(op == 2){return def = 5000;}
+        if(op == 3){return agil = 40;}
+        if(op == 4){hp = 30000;}
+        return 0;
     }
-    public int lapras(){
+    public int lapras(int op){
         nome = "Lapras";
         tipo = "água";
-        return ata = 10000;
-       
+        if(op == 1){return ata = 10000;}
+        if(op == 2){return def = 5000;}
+        if(op == 3){return agil = 50;}
+        if(op == 4){hp = 40000;}
+        return 0;
     }
-    public void maril(){
+    public int maril(int op){
         nome = "Maril";
         tipo = "água";
-        ata = 3000;
-        def = 5000;
-        agil = 40;
-        hp = 40000;
-    }
-    
+        if(op == 1){return ata = 3000;}
+        if(op == 2){return def = 5000;}
+        if(op == 3){return agil = 40;}
+        if(op == 4){hp = 40000;}
+        return 0;
+    }    
 }
